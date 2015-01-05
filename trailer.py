561bab5fe161c68df4f16a5d2b947d95093715f4
gunner <gymgunner@gmail.com>
2015-01-05 23:14:51 +0800

    Change form to dict

--- trailer.py
+++ trailer.py
@@ -1,3 +1,7 @@
+# -*- encoding:utf-8 -*-
+
+import json
+
 from flask import Flask, request
 from flask.ext import restful
 from xunleipy.remote import XunLeiRemote
@@ -24,7 +28,7 @@ class HelloWorld(restful.Resource):
         xlr = XunLeiRemote(u, p)
         peer_list = xlr.get_remote_peer_list()
         pid = peer_list[0]['pid']
-        download_link = request.form['download_link']
+        download_link = json.loads(request.data).get('download_link', '')
         ret = xlr.add_urls_to_remote(pid, 'C:/MovieTrailers/', [download_link])
         print ret
         return ret
